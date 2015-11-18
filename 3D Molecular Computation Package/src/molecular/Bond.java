bb23192ced9794cbb04668ce1de273f141986a28
Thai Nguyen <trevortknguyen@gmail.com>
2015-11-18 00:44:12 -0600

    Finalized Demo for Nov 18
    Added Bonding and modified Element Class to include String id

--- 3D Molecular Computation Package/src/molecular/Bond.java
+++ 3D Molecular Computation Package/src/molecular/Bond.java
@@ -1,13 +1,33 @@
 package molecular;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import simulation.Element;
+
 public class Bond {
 	String id;
 	
-	public Bond(String s) {
-		id = s;
+	Set<Element> elements;
+	
+	public Bond(Element e1, Element e2) {
+		elements = new HashSet<>();
+		elements.add(e1);
+		elements.add(e2);
+	}
+	
+	public boolean equals(Object o) {
+		o = (Bond) o;
+		//add legit implementation later
+		return this.toString().equals(o.toString());
 	}
 	
 	public String toString() {
-		return id;
+		StringBuilder output = new StringBuilder();
+		for (Element e : elements) {
+			output.append(e + " ");
+		}
+		
+		return output.toString();
 	}
 }
--- 3D Molecular Computation Package/src/molecular/MolecularUnit.java
+++ 3D Molecular Computation Package/src/molecular/MolecularUnit.java
@@ -1,15 +1,25 @@
 package molecular;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class MolecularUnit {
-	Set<Bond> bondingTypes;
+import simulation.Updatable;
+
+public class MolecularUnit implements Updatable {
+	private Set<Bond> bondingTypes;
+	private String id;
+	
 	
 	public MolecularUnit() {
-		bondingTypes = new TreeSet<>();
+		bondingTypes = new HashSet<>();
 	}
+	
+	public MolecularUnit(String id) {
+		this();
+		this.id = id;
+	}
+	
 	
 	public boolean append(Bond bond) {
 		return bondingTypes.add(bond);
@@ -18,11 +28,17 @@ public class MolecularUnit {
 	public String toString() {
 		StringBuilder output = new StringBuilder();
 		
-		Iterator iter = bondingTypes.iterator();
+		Iterator<Bond> iter = bondingTypes.iterator();
 		while (iter.hasNext()) {
 			output.append(iter.next() + "\n");
 		}
 		
 		return output.toString();
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
 	}
 }
