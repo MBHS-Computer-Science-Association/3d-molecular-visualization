348fcb23d18da1be82c67b3f0732fa83f39c47d3
SovietBeans <nicholasroth13@gmail.com>
2015-11-30 08:36:02 -0600

    Loader changes

--- 3D Molecular Computation Package/src/data/Loader.java
+++ 3D Molecular Computation Package/src/data/Loader.java
@@ -3,22 +3,24 @@ package data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Loader {
+	
+	static HashMap<Integer, String> table1 = new HashMap<Integer, String>();
 
 	public void readFile(String filePath) {
 
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath));) {
 
 			String currentLine;
-
+			int current = 1;
 			// prints every line of txt file
 			while ((currentLine = br.readLine()) != null) {
 				// what you want it to do with every line
+				table1.put(current, currentLine);
 				System.out.println(currentLine);
 			}
-
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
